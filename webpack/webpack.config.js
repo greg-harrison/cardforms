693995bfd013fe7064cc1d395df4384c193bf9e6
Greg <gharrison@upperquadrant.com>
2017-01-10 08:15:07 -0500

    added some dependencies, still need to get Webpack set up

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-    entry: "./src/index.jsx",
+    entry: './src/index.jsx',
     output: {
         path: '../dist',
-        filename: "bundle.js"
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
-            // { test: /\.css$/, loader: "style!css" }
+            // Processing JS with Babel
+            { 
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/, 
+                loader: 'babel-loader?presets[]=2015&presets[]=react'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css?importLoaders=1!postcss'
+            }
         ]
     }
 };
